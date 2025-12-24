365ea6da6435a3a357270ebaeb5714ffd2ab9d2e
Naz Ikhsan <bluspringindohq@gmail.com>
2025-12-24 23:30:39 +1300

    Trying again?

--- src/main/java/pw/smto/moretools/MoreToolsClient.java
+++ src/main/java/pw/smto/moretools/MoreToolsClient.java
@@ -24,8 +24,11 @@ import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Box;
 import net.minecraft.util.math.Direction;
 import net.minecraft.util.shape.VoxelShapes;
+import org.jetbrains.annotations.NotNull;
+import org.joml.Vector3d;
 import pw.smto.moretools.item.BaseToolItem;
 
+import java.util.List;
 import java.util.Objects;
 
 @Environment(EnvType.CLIENT)
@@ -39,51 +42,84 @@ public class MoreToolsClient implements ClientModInitializer {
             context.client().execute(() -> ClientPlayNetworking.send(new MoreTools.Payloads.C2SHandshakeCallbackWithVersion(MoreToolsClient.VERSION.split("\\+")[0])));
         });
 
-        var directionState = RenderStateDataKey.create();
+        RenderStateDataKey<@NotNull List<BlockPos>> highlightedBlocks = RenderStateDataKey.create();
+        RenderStateDataKey<@NotNull Vector3d> renderPos = RenderStateDataKey.create();
 
         WorldRenderEvents.AFTER_BLOCK_OUTLINE_EXTRACTION.register((context, hit) -> {
+            OutlineRenderState ors = context.worldState().outlineRenderState;
+
+            if (ors == null) return;
+
+            ors.setData(highlightedBlocks, List.of());
+
             if (hit instanceof BlockHitResult blockHit) {
                 if (blockHit.isInsideBlock()) return;
 
-                OutlineRenderState ors = context.worldState().outlineRenderState;
+                // This one too... what the hell is going on?
+                PlayerEntity player = context.gameRenderer().getClient().player;
+                if(player == null) return;
+                if (player.isSpectator()) return;
+                if (player.isSneaking()) return;
 
-                if (ors == null) return;
+                if (player.getEntityWorld().getBlockState(blockHit.getBlockPos()).isAir()) return;
+                ItemStack tool = MoreToolsClient.convertPolymerStack(player.getMainHandStack());
+                if(tool.isEmpty()) return;
+                if (tool.getItem() instanceof BaseToolItem t) {
+                    var blocks = t.getAffectedArea(player.getEntityWorld(), blockHit.getBlockPos(), player.getEntityWorld().getBlockState(blockHit.getBlockPos()), blockHit.getSide(), player.getEntityWorld().getBlockState(blockHit.getBlockPos()).getBlock());
+                    if (blocks == null || blocks.isEmpty()) return;
 
-                ors.setData(directionState, blockHit.getSide());
+                    double d0 = player.lastRenderX + (player.getX() - player.lastRenderX) * MinecraftClient.getInstance().getRenderTickCounter().getTickProgress(true);
+                    double d1 = player.lastRenderY + player.getStandingEyeHeight() + (player.getY() - player.lastRenderY) * MinecraftClient.getInstance().getRenderTickCounter().getTickProgress(true);
+                    double d2 = player.lastRenderZ + (player.getZ() - player.lastRenderZ) * MinecraftClient.getInstance().getRenderTickCounter().getTickProgress(true);
+
+                    ors.setData(highlightedBlocks, blocks);
+                    ors.setData(renderPos, new Vector3d(d0, d1, d2));
+                }
             }
         });
 
-        WorldRenderEvents.BEFORE_BLOCK_OUTLINE.register((context, hitResult) -> {
-            if (context.matrices() == null) return true;
-            if (context.consumers() == null) return true;
+        Vector3d zero = new Vector3d();
 
-            // This one too... what the hell is going on?
-            PlayerEntity player = context.gameRenderer().getClient().player;
-            if(player == null) return true;
-            if (player.isSpectator()) return true;
-            if (player.isSneaking()) return true;
+        WorldRenderEvents.BEFORE_BLOCK_OUTLINE.register((context, renderState) -> {
+            var blocks = renderState.getDataOrDefault(highlightedBlocks, List.of());
 
-            if (player.getEntityWorld().getBlockState(hitResult.pos()).isAir()) return true;
-            ItemStack tool = MoreToolsClient.convertPolymerStack(player.getMainHandStack());
-            if(tool.isEmpty()) return true;
-            if (tool.getItem() instanceof BaseToolItem t) {
-                var blocks = t.getAffectedArea(player.getEntityWorld(), hitResult.pos(), player.getEntityWorld().getBlockState(hitResult.pos()), (Direction) context.worldState().outlineRenderState.getData(directionState), player.getEntityWorld().getBlockState(hitResult.pos()).getBlock());
-                if(blocks == null || blocks.isEmpty()) return true;
+            if (!blocks.isEmpty()) {
+                Vector3d pos = renderState.getDataOrDefault(renderPos, zero);
 
-                double d0 = player.lastRenderX + (player.getX() - player.lastRenderX) * MinecraftClient.getInstance().getRenderTickCounter().getTickProgress(true);
-                double d1 = player.lastRenderY + player.getStandingEyeHeight() + (player.getY() - player.lastRenderY) * MinecraftClient.getInstance().getRenderTickCounter().getTickProgress(true);
-                double d2 = player.lastRenderZ + (player.getZ() - player.lastRenderZ) * MinecraftClient.getInstance().getRenderTickCounter().getTickProgress(true);
-
-                for(BlockPos block : blocks) {                    VertexRendering.drawOutline(
-                            Objects.requireNonNull(context.matrices()),
-                            Objects.requireNonNull(context.consumers()).getBuffer(RenderLayers.lines()),
-                            VoxelShapes.cuboid(new Box(block).offset(-d0, -d1, -d2)),
-                            1, 1, 1, 0xFFFFFF, 0.4F
+                for(BlockPos block : blocks) {
+                    VertexRendering.drawOutline(
+                        context.matrices(),
+                        context.consumers().getBuffer(RenderLayers.lines()),
+                        VoxelShapes.cuboid(new Box(block).offset(-pos.x(), -pos.y(), -pos.z())),
+                        1, 1, 1, 0xFFFFFF, 0.4F
                     );
                 }
+
                 return false;
             }
+
             return true;
+        });
+
+        WorldRenderEvents.END_MAIN.register(context -> {
+            OutlineRenderState renderState = context.worldState().outlineRenderState;
+
+            if (renderState == null) return;
+
+            var blocks = renderState.getDataOrDefault(highlightedBlocks, List.of());
+
+            if (!blocks.isEmpty()) {
+                Vector3d pos = renderState.getDataOrDefault(renderPos, new Vector3d());
+
+                for(BlockPos block : blocks) {
+                    VertexRendering.drawOutline(
+                        context.matrices(),
+                        context.consumers().getBuffer(RenderLayers.lines()),
+                        VoxelShapes.cuboid(new Box(block).offset(-pos.x(), -pos.y(), -pos.z())),
+                        1, 1, 1, 0xFFFFFF, 0.4F
+                    );
+                }
+            }
         });
     }
 
